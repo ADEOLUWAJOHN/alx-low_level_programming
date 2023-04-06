9cd218f894b6b58825636486ca49b04ac647211c
Your Name <you@example.com>
2023-04-05 20:52:48 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+
+#include <studio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string to be printed.
+ *
+ * Return: void.
+ */
+
+void _puts_recursion(char *s)
+
+{
+	if (*s == '\0')
+	{
+		putchar('\n'); /* print new line when string is empty */
+		return;
+	}
+	putchar(*s); /* print the first character of the string */
+	/* call _puts_recursion recursively with the next character of the string */
+	_puts_recursion(s + 1);
+}
