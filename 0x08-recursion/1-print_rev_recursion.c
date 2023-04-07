48e50f4106dae1d00b1ec48ea8d244abbf0f2ecc
Your Name <you@example.com>
2023-04-06 17:31:44 -0700

    PROGRESS

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,6 +4,13 @@
 
 /**
  *  _print_rev_recrsion - Prints a string in reverse.
+ *
+ *  Description: This function takes a string as an
+ *  input and recursively prints it in reverse order,
+ *  it starts by calling itself with the next character
+ *  in the string, and once the end of the string is reached,
+ *  it prints the characters in reverse order.
+ *
  *  @s: Pointer to the string to point.
  */
 void _print_rev_recursion(char *s)
