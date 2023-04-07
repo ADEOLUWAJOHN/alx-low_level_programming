c0852b27f000be7930dd7b7e93f3ca11941074c5
Your Name <you@example.com>
2023-04-06 18:05:17 -0700

    1. Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,9 +8,8 @@
  *
  *  Description: This function takes a string as an
  *  input and recursively prints it in reverse order,
- *  it starts by calling itself with the next character
- *  in the string, and once the end of the string is reached,
- *  it prints the characters in reverse order.
+ *  starting from the last character in the string
+ *  and ending with the first character.
  *
  *  @s: Pointer to the string to point.
  */
