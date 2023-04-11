17e2ab8d01fb3f4f96cc5bea1c2efcc064e3f034
Your Name <you@example.com>
2023-04-10 21:28:32 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,55 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
- * is_digit - checks if a string is a valid positive digit
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * @str: the string to check
- *
- * Return: 1 if @str is a valid positive digit, 0 otherwise
- */
-int is_digit(char *str)
-{
-	while (*str)
-	{
-		if (*str < '0' || *str > '9')
-			return (0);
-		str++;
-	}
-
-	return (1);
-}
-
-/**
- * main - entry point
- *
- * @argc: the number of arguments passed to the program
- * @argv: an array of strings containing the arguments
- *
- * Return: 0 if the program ran successfully, 1 otherwise
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0;
+	int num, digit, sum = 0;
 
-	if (argc == 1)
+	for (num = 1; num < argc; num++)
 	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (int i = 1; i < argc; i++)
-	{
-		if (!is_digit(argv[i]))
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 
-		sum += atoi(argv[i]);
+		sum += atoi(argv[num]);
 	}
 
+
 	printf("%d\n", sum);
+
+
 	return (0);
 }
