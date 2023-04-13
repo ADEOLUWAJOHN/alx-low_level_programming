60069bddc9a9cc26ff9c9decce5493352a00939d
Your Name <you@example.com>
2023-04-12 19:36:42 -0700

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - creates a duplicate of a string
+ * @str: the string to duplicate
+ *
+ * Return: a pointer to the duplicated string, or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	char *dup_str;
+	int length = 0;
+
+
+	if (str == NULL)
+		return (NULL);
+
+
+	while (str[length] != '\0')
+		length++;
+
+
+	dup_str = malloc(sizeof(char) * (length + 1));
+	if (dup_str == NULL)
+		return (NULL);
+
+	for (int i = 0; i <= length; i++)
+		dup_str[i] = str[i];
+	return (dup_str);
+}
